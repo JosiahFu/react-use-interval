0b577e2809d126ae4a6dfda83836b30c759ef559
JosiahFu <josiahfu@gmail.com>
2023-08-20 23:05:37 -0700

    Update documentation, add parameter

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # `react-use-interval`
+
+React hook to statefully set an interval. Will update when the function updates and has proper cleanup.
+
+## Example Usage
+
+```ts
+useInterval(
+    // Highly reccommend to use a useCallback hook so that the interval isn't
+    // changed every single render
+    useCallback(() => {
+        console.log('Interval');
+        // ...
+    }, []),
+    1000
+);
+```
+
+```ts
+const [message, setMessage] = useState('Test');
+
+useInterval(
+    useCallback(() => {
+        console.log(message);
+    }, [message]),
+    60000,
+    // Determines if the function should run on initialization, otherwise it
+    // will not run until the first interval
+    false, 
+    // If resetTimerOnChange is set to true, the timer will reset when the
+    // function changes
+    true 
+);
+```
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import { useEffect, useRef } from 'react';
 
-function useInterval(callback: () => void, timeout: number, resetTimerOnChange = false) {
+/**
+ * Sets an interval (running a task repeatedly)
+ *
+ * @param callback The function to run on the interval. It is recommended to use the {@link React.useCallback|useCallback} hook around this argument to prevent changing the function frequently.
+ * @param timeout The time between executions.
+ * @param runOnInit Whether or not `callback` should be run on initialization. If false, it will not run until after the first delay. True by default.
+ * @param resetTimerOnChange Whether changing the callback should reset the timer. If `runOnInit` is true and this is true, this will call the function immediately when it changes.
+ */
+function useInterval(callback: () => void, timeout: number, runOnInit = true, resetTimerOnChange = false) {
     const callbackRef = useRef(callback);
 
     const currentCallback = resetTimerOnChange ? callback : callbackRef.current;
@@ -10,9 +18,10 @@ function useInterval(callback: () => void, timeout: number, resetTimerOnChange =
     }, [callback]);
 
     useEffect(() => {
+        if (runOnInit) currentCallback();
         const id = setInterval(currentCallback, timeout);
         return () => clearInterval(id);
-    }, [currentCallback, timeout]);
+    }, [currentCallback, runOnInit, timeout]);
 }
 
 export { useInterval };
